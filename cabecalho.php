571dffb90dac67fc8b99c567e045c8c1af1abea8
ArthurPedralli <arthurpedralli@gmail.com>
2015-05-02 22:47:22 -0300

    Importações
    importando bootstrap, jquery, etc..

---
+++ cabecalho.php
@@ -0,0 +1,20 @@
+<?php
+	include("conexao.php"); 
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Projeto Arthur</title>
+	<link rel="stylesheet" type="text/css" href="./lib/bootstrap/css/bootstrap.min.css">
+	<script src="./lib/jquery/jquery.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="./lib/font/css/font.min.css"></link>
+	<style type="text/css">
+	.tabela{
+		margin-top: 3%;
+	}
+	</style>
+
+</head>
+<body>
+	<div class="container">
---
+++ rodape.php
@@ -0,0 +1,7 @@
+</div>
+<script src="./lib/datatable/media/js/jquery.dataTables.min.js"></script>
+<script src="./lib/datatable/dataTables.bootstrap.js"></script>
+<script src="./lib/bootstrap/js/bootstrap.min.js"> </script>
+<script src="./lib/jqueryvalidate/dist/jquery.validate.min.js"></script>
+</body>
+</html> 
