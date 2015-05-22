9a3fa07a5922a06a74719e74a38e816797f4b53d
arthur.minardi <arthur.minardi@unicesumar.edu.br>
2015-05-22 15:43:32 -0300

    Atualizações
    Index: ajustando algumas coisas
    Menu: trocnado por um responsivo

--- index.php
+++ index.php
@@ -11,36 +11,34 @@ include("menu.php");
 		<div class="container-fluid">
 			<div class="tabela">
 				<div class="row">
-					<div class="col-md-12">	
+					<div class="col-md-12 form-group">	
 						<div class="col-md-6">
 				        	<label for="txtOrigem">Endereço de origem: </label>
-		        			<input placeholder = "Rio de Janeiro - RJ / Rua da Conceição, Rio de Janeiro - RJ" type="text" style="width: 100%"  class="form-control" id="txtDestino" />
+		        			<input placeholder = "Rio de Janeiro - RJ / Rua da Conceição, Rio de Janeiro - RJ" type="text"  class="form-control" id="txtDestino" />
 						</div>	
 						<div class="col-md-6">
 						    <label for="txtDestino">Endereço de destino: </label>
-				        	<input placeholder = "São Paulo - SP / Rua Oscar Freire, São Paulo - SP" type="text" id="txtOrigem" class="form-control" style="width: 100%" />
+				        	<input placeholder = "São Paulo - SP / Rua Oscar Freire, São Paulo - SP" type="text" id="txtOrigem" class="form-control" />
 						</div>
 					</div>
 				</div>
-
+				
 				<div class="row">
-					<div class="col-md-12">
+					<div class="col-md-12 form-group">
 						<div class="col-md-6">
 							<label for="txtConsumo">Consumo do veículo (KM/L): </label>
-							<input type="text" id="txtConsumo" class="form-control" style="width: 100%" />
+							<input type="text" id="txtConsumo" class="form-control" />
 						</div>
 						<div class="col-md-6">
 							<label for="txtPrecoCombustivel">Preço do combustível (R$): </label>
-							<input  type="text" id="txtPrecoCombustivel" class="form-control" style="width: 100%" />
+							<input  type="text" id="txtPrecoCombustivel" class="form-control" />
 						</div>
 					</div>
 				</div>
-
+				
 				<div class="row">
-					<div class="col-md-12">	
-						<div class="botaoConfirmacao">
-							<input type="button" value="Calcular distância" onclick="CalculaDistancia()" class="btn btn-success" />
-						</div>
+					<div class="col-md-12 form-group">	
+						<input type="button" value="Calcular distância" onclick="CalculaDistancia()" class="btn btn-success" />
 					</div>
 				</div>
 
@@ -50,9 +48,9 @@ include("menu.php");
 				</div>
 
 				<div class="row">
-					<div class="col-md-12">
+					<div class="col-md-12 form-group">
 						<div class="mapa" >
-				        	<iframe width="100%" scrolling="no" height="500" frameborder="0" id="map" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?output=embed"></iframe>
+				        	<iframe width="100%" scrolling="no" height="450" frameborder="0" id="map" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?output=embed"></iframe>
 				        	<!----<div id="map" style="width:100%; height:100%" onload="initialize()"></div> -->
 				    	</div>
 					</div>
--- menu.php
+++ menu.php
@@ -5,7 +5,39 @@
 	<link type="text/css" rel="stylesheet" href="./css/menu.css"/>
 </head>
 <body>
-	<div class="row">
+	<nav class="navbar navbar-default navbar-fixed-top">
+	    <div class="container">
+	        <div class="navbar-header">
+	          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+	            <span class="sr-only">Toggle navigation</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+	          <a class="navbar-brand" href="./index.php">Calcule sua Rota</a>
+	        </div>
+	        <div id="navbar" class="navbar-collapse collapse">
+	          <ul class="nav navbar-nav">
+	            <!--<li><a href="./dicasSeguranca.php">Dicas de Segurança</a></li>
+	             <li class="active"><a href="#">Home</a></li>
+	            <li><a href="#about">About</a></li>
+	            <li><a href="#contact">Contact</a></li>-->
+	            <li class="dropdown">
+	              <a href="./dicasSeguranca.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Segurança<span class="caret"></span></a>
+	              <ul class="dropdown-menu" role="menu">
+	                <li><a href="./dicasSeguranca.php">Another action</a></li>
+	                <li><a href="#">Something else here</a></li>
+	                <li class="divider"></li>
+	                <li class="dropdown-header">Nav header</li>
+	                <li><a href="#">Separated link</a></li>
+	                <li><a href="#">One more separated link</a></li>
+	              </ul>
+	            </li>
+	          </ul>
+	        </div>
+	    </div>
+	</nav> 
+	 <!-- <div class="row">
 		<div class="col-md-12">
 			<nav class="navbar navbar-default navbar-fixed-top">
 				<div class="container-fluid">
@@ -14,19 +46,19 @@
 					</div>
 					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 						<ul class="nav navbar-nav">
-							<!--<li><a href="./videosSeguranca.php">Vídeos sobre Segurança</a></li>-->
+							<li><a href="./videosSeguranca.php">Vídeos sobre Segurança</a></li>
 							<li><a href="./dicasSeguranca.php">Dicas de Segurança</a></li>
-							<!-- <form class="navbar-form navbar-left" role="search" method="POST">
+							<form class="navbar-form navbar-left" role="search" method="POST">
 								<div class="form-group">
 									<input type="text" class="form-control" name="procura" id="procura" placeholder="Search">
 								</div>
 								<input class="btn btn-default" type="submit" value="Procurar">
-							</form> -->
+							</form> 
 						</ul>
 					</div>
 				</div>
 			</nav>
-		</div>
-	</div>
+		</div> 
+	</div> -->
 </body>
 </html>
