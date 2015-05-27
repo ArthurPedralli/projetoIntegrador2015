ab2336866606e229971e27363d4f70bf9d2bfd5e
arthur.minardi <arthur.minardi@unicesumar.edu.br>
2015-05-27 12:12:13 -0300

    Atualizações

--- DicasSeguranca.php
+++ DicasSeguranca.php
@@ -6,30 +6,21 @@ include("menu.php");
 		<link type="text/css" rel="stylesheet" href="./css/dicasSeguranca.css"/>
 
 		<div class="cabecalheDiscasDeSeguranca">
-			<div class="jumbotron">
-				<h2>Dicas de segurança calculesuarota.com</h2>
-				<p>Dicas de segurança calculesuarota.com</p>
+			<div class="col-md-12">
+				<h2>REDOBRE A ATENÇÃO AO DIRIGIR À NOITE!</h2>
+				<p><h4>Dirigir no período da noite requer atenção redobrada dos motoristas. 
+				Os perigos estão relacionados à iluminação, cansaço, condições das estradas e imprudência. 
+				Em razão do menor número de veículos circulando à noite, muitos condutores aproveitam para dirigir em alta velocidade. 
+				Algumas condições adversas, que seriam mais facilmente identificadas durante o dia, ficam escondidas nesse período, como buracos na estrada e até mesmo animais cruzando a pista. 
+				Por isso, dirigir em alta velocidade aumenta as chances de que o condutor não perceba ou veja muito em cima da hora esses perigos e acabe envolvido em um acidente.
+				Muitas rodovias têm grandes trechos sem iluminação pública, o que faz com que o condutor tenha que se guiar somente por seus faróis. É muito importante manter as luzes do carro sempre em boas condições para que todos possam ver e serem vistos.
+				É preciso também que o motorista use corretamente a regulagem dos faróis baixos, pois existem condutores que podem ficar cegos por um curto período de tempo quando encaram uma luz brilhante nos olhos, aumentando a chance de um acidente. O farol alto só deve ser usado quando realmente necessário, para não prejudicar os outros.
+				Outro ponto de extrema importância é o cansaço. No período da noite a probabilidade de que o motorista durma ao volante é muito mais alta. Por isso, as viagens à noite devem ser programadas para que o condutor tenha momentos de descanso. Se estiver dirigindo e se sentir cansado, pare e descanse! Você pode dormir sem nem ao menos perceber e colocar a sua vida e dos demais usuários das vias em risco.
+				A prevenção é a maior aliada da segurança! Seja prudente e preserve a vida!</h4></p>
 				<button class="btn btn-primary">Assista o Vídeo!</button>
 			</div>
 		</div>
 
-		<!-- <table width="100%" border="1">
-			<tr>
-				<td><p>Transporte seguro de crianças.</p></td>
-				<td>2</td>
-			</tr>
-
-			<tr>
-				<td>3</td>
-				<td>4</td>
-			</tr>
-
-			<tr>
-				<td>5</td>
-				<td>6</td>
-			</tr>
-			
-		</table> -->
 
 
 <?php
