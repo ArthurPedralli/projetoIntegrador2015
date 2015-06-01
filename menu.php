692c086843b6114abdb4776cc7c5ad8900a69218
ArthurPedralli <arthurpedralli@gmail.com>
2015-05-31 22:32:26 -0300

    Atualizações
    Modal em Dúvidas e Sugestões

--- menu.php
+++ menu.php
@@ -29,12 +29,86 @@
 		            </li>
 	          	</ul>
 	            <ul class="nav navbar-nav navbar-right">
-	            	<li><a href="./duvidasSugestoes.php">Dúvidas e Sugestões</a></li>
+	            	<li><a class="link" data-toggle="modal" data-target="#myModal">Dúvidas e Segustões</a></li>
 	            </ul>
 	        </div>
 	    </div>
-	    
 	</nav> 
 	 
+	 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <form id="myform" >
+            <input type="hidden" name="idProfessor" id="idProfessor" value="">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+              <h4 class="modal-title" id="myModalLabel">Dúvidas e Sugestões</h4>
+            </div>
+            <div class="modal-body">
+              <div class="form-group">
+                <label for="nomeUsuario">Nome</label>
+                <input type="text" class="form-control" id="nomeUsuario" name="nomeUsuario" placeholder="Nome: ">
+              </div>
+              <div class="form-group">
+                <label for="emailUsuario">Email</label>
+                <input type="text" class="form-control" id="emailUsuario" name="emailUsuario" placeholder="Email: ">   
+              </div>
+              <div class="bs-example" data-example-id="block-checkboxes-radios">
+                <label>Escolha: </label>
+                <div class="radio">
+                  <label>
+                    <input type="radio" name="status" id="optionsRadios1" value="duvida" checked> Dúvida
+                  </label>
+                </div>
+                <div class="radio">
+                  <label>
+                    <input type="radio" name="status" id="optionsRadios2" value="sugestao"> Sugestão
+                  </label>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="textoUsuario">Texto</label>
+                <textarea class="form-control" rows="3" id="textoUsuario" name="textoUsuario"></textarea> 
+              </div>
+            </div>
+            <!-- <div class="form-group">
+              <label class="col-sm-3 col-md-2 control-label" for="captcha">
+                <span class="text-danger">*</span> Verificação:
+              </label>
+              <div class="col-sm-9 col-md-10">
+                <div class="captcha well well-sm text-center">
+                  <span class="help-block">Por favor, complete o campo abaixo com os caracteres exibidos na imagem:</span>
+                  <img src="./captcha/captcha.php" title="Clique para atualizar a imagem" alt="Clique para atualizar a imagem" onclick="this.src=this.src" style="cursor: pointer;"><br>
+                  <input id="captcha" name="captcha" placeholder="Digite o texto" type="text" maxlength="5" required>
+                </div>
+              </div>
+            </div>-->
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+              <form action="#myModal1">
+                <button type="submit" class="btn btn-primary">Salvar</button>
+              </form>
+            </div>
+
+          </form>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <form id="myform" >
+
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+              <button type="submit" id="envia_dica_sugestao" class="btn btn-primary">Salvar</button>
+            </div>
+
+          </form>
+        </div>
+      </div>
+    </div>
 </body>
 </html>
