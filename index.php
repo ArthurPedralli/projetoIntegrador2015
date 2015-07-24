eae2617342bda2a385d8fe0538af5d1371004811
Vinícius Baesso <vinicius.baesso@gmail.com>
2015-07-23 21:54:43 -0300

    att

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ include("menu.php");
 				<div class="row">
 					<div class="col-md-12 form-group">	
 						<div class= "col-md-6">
-							<input type="button" value="Calcular distância" onclick="CalculaDistancia()" class="btn btn-success" data-target="#myModal" />
+							<input type="button" value="Calcular distância" onclick="CalculaDistancia()" class="btn btn-success" data-target="#mapa" />
 <!-- 						<button type="button" id="myButton" data-loading-text="Loading..." class="btn btn-primary" onclick="CalculaDistancia()" autocomplete="off">Calcular</button>-->						</div>
 					</div>
 				</div>
