e7cee91a54a7ed02946818854839c04361640c91
arthur.minardi <arthur.minardi@unicesumar.edu.br>
2015-09-02 09:19:30 -0300

    Atualizando PDF

--- geraPDF.php
+++ geraPDF.php
@@ -1,25 +1,30 @@
-<?php  
+<?php
 	include_once("lib/mpdf60/mpdf.php");
+/*
+	if (true) {
+		$nome_arq="Calculo_Distancia";
+		header("Content-type: application/force-download");
+		header("Content-Disposition: attachment; filename=$nome_arq.pdf");
+		header("Pragma: no-cache");
+	}
+*/
 
-	/*echo "<iframe width='100%' scrolling='no' height='450' frameborder='0' id='map' marginheight='0' marginwidth='0'  
-	src='https://maps.google.com/maps?output=embed'></iframe>";*/
 
-	echo "<iframe width='50%' scrolling='no' height='50%' frameborder='0' id='map' marginheight='0' marginwidth='0'  
+	$origem 			= $_REQUEST['origem'];
+	$destino 			= $_REQUEST['destino'];
+	$preco 				= $_REQUEST['preco'];
+	$consumo 			= $_REQUEST['consumo'];
+	$totalCombustivel 	= $_REQUEST['totalCombustivel'];
+
+	echo "<p style='margin-left: 25%;'><b>Origem:</b> ".utf8_decode($origem)." </p>";
+	echo "<p style='margin-left: 25%;'><b>Destino:</b> ".utf8_decode($destino)." </p>";
+	echo "<p style='margin-left: 25%;'><b>Preço Combustível:</b> ".utf8_decode($preco)." </p>";
+	echo "<p style='margin-left: 25%;'><b>Consumo:</b> ".utf8_decode($consumo)." </p>";
+	echo "<p style='margin-left: 25%;'> ".utf8_decode($totalCombustivel)." </p>";
+
+	echo "<iframe  width='50%' height='60%' scrolling='no' style='margin-left: 25%;' frameborder='0' id='map' marginheight='0' marginwidth='0'
 	src='https://maps.google.com/maps?saddr=".$_REQUEST['origem']."&daddr=".$_REQUEST['destino']."&output=embed'></iframe>";
 
-	$origem = "Origem: ".$_REQUEST['origem'];
-	$destino = "Destino: ".$_REQUEST['destino'];
-	$preco = "PreÃ§o: ".$_REQUEST['preco'];
-	$consumo = "Consumo: ".$_REQUEST['consumo']; 
-	$totalCombustivel = $_REQUEST['totalCombustivel'];
-
-	echo "<pre>".print_r($origem, true)."</pre>";
-	echo "<pre>".print_r($destino, true)."</pre>";
-	echo "<pre>".print_r($preco, true)."</pre>";
-	echo "<pre>".print_r($consumo, true)."</pre>";
-	echo "<pre>".print_r($totalCombustivel, true)."</pre>";
-		
-		
 
 	/*$mpdf = new mPDF();
 	$mpdf->WriteHTML($origem);
