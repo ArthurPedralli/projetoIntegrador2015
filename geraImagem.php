8e7a971c367f7607548bd480fc913e319a6e83e5
ArthurPedralli <arthurpedralli@gmail.com>
2015-11-01 22:40:16 -0200

    Atualização

--- geraImagem.php
+++ geraImagem.php
@@ -2,7 +2,7 @@
 	$im = imagegrabscreen();
 	imagepng($im, "imagem.png");
 	//$to_crop_array = array('x' =>85 , 'y' => 120, 'width' => 1190, 'height'=> 680);
-	$to_crop_array = array('x' =>70 , 'y' => 111, 'width' => 1000, 'height'=> 560);
+	$to_crop_array = array('x' =>70 , 'y' => 120, 'width' => 1150, 'height'=> 560);
 	$thumb_im = imagecrop($im, $to_crop_array);
 	imagepng($thumb_im, 'imagem.png');
 	header('Location: http://localhost/projetoIntegrador2015/geraPDF.php');
